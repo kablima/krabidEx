fb0896321b3c333de750c229b9eadf261e19123f
kablima <shopdaden@gmail.com>
2026-03-12 22:49:54 -0500

    test: add ERC20 metadata and transfer tests

--- backend/test/Token.js
+++ backend/test/Token.js
@@ -6,12 +6,30 @@ describe("Token", function () {
     const { ethers } = await network.connect();
     const initialSupply = ethers.parseUnits("1000000", 18);
     const token = await ethers.deployContract("Token", [initialSupply]);
-    return { token, initialSupply, ethers };
+    const [owner, addr1] = await ethers.getSigners();
+    
+    return { token, initialSupply, owner, addr1, ethers };
   }
+  
   it("sets the total supply to the deployer", async function () {
-    const { token, initialSupply, ethers } = await deployTokenFixture();
-    const [owner] = await ethers.getSigners();
+    const { token, initialSupply, owner } = await deployTokenFixture();
     expect(await token.totalSupply()).to.equal(initialSupply);
     expect(await token.balanceOf(owner.address)).to.equal(initialSupply);
+  });
+  
+  it("sets the correct token name and symbol", async function () {
+    const { token } = await deployTokenFixture();
+    expect(await token.name()).to.equal("Moneda");
+    expect(await token.symbol()).to.equal("MON");
+  });
+  
+  it("transfers tokens between accounts", async function () {
+    const { token, owner, addr1, ethers } = await deployTokenFixture();
+    const transferAmount = ethers.parseUnits("100", 18);
+    await token.transfer(addr1.address, transferAmount);
+    expect(await token.balanceOf(addr1.address)).to.equal(transferAmount);
+    expect(await token.balanceOf(owner.address)).to.equal(
+      (await token.totalSupply()) - transferAmount
+    );
   });
 });
